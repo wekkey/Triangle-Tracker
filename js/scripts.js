8fad34271610c9704a54688ad9dd568da4a868c4
wekkey <stellzwetkke@gmail.com>
2019-03-25 12:31:19 +0300

    Declare the variables for l, w and h.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 
 function triangleTracker(){
+  var l = parseInt(document.getElementById("l").value);
+  var w = parseInt(document.getElementById("w").value);
+  var h= parseInt(document.getElementById("h").value);
 
-    
   }
