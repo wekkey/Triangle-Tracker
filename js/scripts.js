6c83f98221d07c2665487609c881ab23108ad291
wekkey <stellzwetkke@gmail.com>
2019-03-25 12:32:45 +0300

    Add an if statement to determine if the triangle is equilaterla.

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,9 @@ function triangleTracker(){
   var l = parseInt(document.getElementById("l").value);
   var w = parseInt(document.getElementById("w").value);
   var h= parseInt(document.getElementById("h").value);
+    if (l === w && w === h && l === h) {
+      alert("Equilateral Triangle");
+  }
+    
 
   }
