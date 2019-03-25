4d68c742cbb193e67dd862d0dfc0df0b54707ab6
wekkey <stellzwetkke@gmail.com>
2019-03-25 12:30:07 +0300

    Declare the triangleTracker function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,5 @@
 
 function triangleTracker(){
 
-    var l = parseInt(document.getElementById("l").value);
-    var w = parseInt(document.getElementById("w").value);
-    var h= parseInt(document.getElementById("h").value);
-
-        if (l === w && w === h && l === h) {
-          alert("Equilateral Triangle");
-      }
-        else if (l === w || w === h || l === h ) {
-          alert("Isosceles Triangle");
-        }
-
-        else if ((l+w)<=h || (l+h)<=w || (w+h)<=l) {
-          alert(" Not a Triangle");
-      }
-        else if (l !== w && w !== h ) {
-          alert("Scalene Triangle");
-      }
+    
   }
